32d357ee9ada9a8fa0b3570ba6061589300f1540
Gustavo Pianaro <123005342+Narozera@users.noreply.github.com>
2023-09-29 12:21:02 -0300

    linkedin link

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,47 +1,53 @@
 export const services = [
-    {
-        id: 1,
-        title: "Direito do Trabalho",
-        icon: "/wallet.svg",
-        alt: "Mala",
-        link: "/atuação/direito-do-trabalho#texto",
-    },  
-    {
-        id: 2,
-        title: "Direito do Consumidor",
-        icon: "/light.svg",
-        alt: "Lâmpada",
-        link: "/atuação/direito-do-consumidor#texto"
-    },  
-    {
-        id: 3,
-        title: "Direito Empresarial",
-        icon: "/company.svg",
-        alt: "Empresa",
-        link: "/atuação/direito-empresarial#texto"
-    },  
-    {
-        id: 4,
-        title: "Direito de Família",
-        icon: "/family.svg",
-        alt: "Família",
-        link: "/atuação/direito-de-família#texto"
-    },  
-    {
-        id: 5,
-        title: "Direito Bancário",
-        icon: "/bank.svg",
-        alt: "Banco",
-        link: "/atuação/direito-bancário#texto"
-    },  
-    {
-        id: 6,
-        title: "Direito Digital",
-        icon: "/wifi.svg",
-        alt: "Wifi",
-        link: "/atuação/direito-digital#texto"
-    },  
-]
+  {
+    id: 1,
+    title: "Direito do Trabalho",
+    icon: "/wallet.svg",
+    alt: "Mala",
+    link: "/atuação/direito-do-trabalho#texto",
+  },
+  {
+    id: 2,
+    title: "Direito do Consumidor",
+    icon: "/light.svg",
+    alt: "Lâmpada",
+    link: "/atuação/direito-do-consumidor#texto",
+  },
+  {
+    id: 3,
+    title: "Direito Empresarial",
+    icon: "/company.svg",
+    alt: "Empresa",
+    link: "/atuação/direito-empresarial#texto",
+  },
+  {
+    id: 4,
+    title: "Direito de Família",
+    icon: "/family.svg",
+    alt: "Família",
+    link: "/atuação/direito-de-família#texto",
+  },
+  {
+    id: 5,
+    title: "Direito Bancário",
+    icon: "/bank.svg",
+    alt: "Banco",
+    link: "/atuação/direito-bancário#texto",
+  },
+  {
+    id: 6,
+    title: "Direito Digital",
+    icon: "/wifi.svg",
+    alt: "Wifi",
+    link: "/atuação/direito-digital#texto",
+  },
+];
 
-export const links = ["https://api.whatsapp.com/send?phone=5511948422225", "mailto:contato@alexandro.com.com.br.com.br?subject=&body=", "loc", "linkedin", "https://www.facebook.com/dr.alexsandrogomes", "https://www.instagram.com/dr.alexsandrogomes/"
-]
+export const links = [
+  "https://api.whatsapp.com/send?phone=5511948422225",
+  "mailto:contato@alexandro.com.com.br.com.br?subject=&body=",
+  "loc",
+  "http://linkedin.com/in/adv-alexsandro-gomes",
+  "https://www.facebook.com/dr.alexsandrogomes",
+  "https://www.instagram.com/dr.alexsandrogomes/",
+];
